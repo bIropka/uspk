37e520f543b88a015469e70641cf18bdb5487d46
bIropka <BrDm_100>
2016-09-02 01:40:30 +0300

    v 0.2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -29,76 +29,9 @@ $(document).ready(function () {
         }
     });
 
-    $('.video-gallery').lightGallery();
-
-    $('.timer ul').downCount({
-        date: '08/27/2016 00:00:00',
-        offset: +3
-    });
-
-    var headerBottom = $('.header-bottom');
-
-    $(window).scroll(function() {
-
-        if ($(window).width() > '980'){
-
-            var blockPosition;
-            if(headerBottom.hasClass('to-top')) {
-                blockPosition = $('.unique-helper').offset().top - $(window).scrollTop();
-                if (blockPosition >= 0) {
-                    $(headerBottom).removeClass('to-top');
-                }
-            } else {
-                blockPosition = $('.header-bottom').offset().top - $(window).scrollTop();
-                if (blockPosition <= 0) {
-                    $(headerBottom).addClass('to-top');
-                }
-            }
-
-        }
-
-    });
-
     /******************************
      ******* other scripts ********
      ******************************/
-
-    $('.custom-select .current-value').click(function() {
-        if ($(this).hasClass('active')) {
-            $('.custom-select .current-value').removeClass('active');
-            $('.custom-select ul').slideUp();
-        } else {
-            $('.custom-select .current-value').removeClass('active');
-            $('.custom-select ul').slideUp();
-            $(this).toggleClass('active');
-            $(this).next().slideToggle();
-        }
-
-    });
-
-    $('.custom-select ul li').click(function() {
-        $(this).parent().slideUp();
-        $(this).parent().siblings('.current-value').removeClass('active');
-        var value = $(this).html();
-        var array = value.split('>');
-        $(this).parent().siblings('.current-value').html(value);
-        $(this).parent().siblings('input').val(array[array.length - 1]);
-    });
-
-    $('.custom-checkbox').on('change', function() {
-        if(this.checked) {
-            $(this).parents('form').find('.current-sum').html('7499 руб.');
-            $(this).attr('value', '7499');
-        } else {
-            $(this).parents('form').find('.current-sum').html('5999 руб.');
-            $(this).attr('value', '5999');
-        }
-    });
-
-    $('.burger').click(function() {
-        $('.header-bottom').toggleClass('active');
-        $(this).toggleClass('active');
-    });
 
     $(function($) {
         $('form').validatr({
@@ -161,24 +94,34 @@ $(document).ready(function () {
     $('input').on('focus', function() {
         $(this).removeClass('valid-field invalid-field');
     });
+
     $('input[type="tel"]').mask("+7 ( 999 ) 999 - 99 - 99");
     $('input[type="tel"]').click(function() {
         $(this).focus();
     });
 
-    $('.icon-question').hover(function() {
-        if ($(window).width() > '980'){
-            $(this).find('.tooltip').toggleClass('active');
-        }
-    });
+    /*******************************
+     ******* slider scripts ********
+     ******************************/
 
-    $('.icon-question').click(function() {
-        if ($(window).width() < '981'){
-            $(this).find('.tooltip').toggleClass('active');
-        }
-    });
+    $('.slider-logisticians').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
 
-    $('#background-video').tubular({videoId: 'GJMuafDEFt0'});
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+
+                }
+            }
+        ]
+    });
 
 });
 
